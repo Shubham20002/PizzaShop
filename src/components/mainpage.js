543ac8ec14c63c5b1280ea2708483348d474c8ed
shubham <shubham992284@gmail.com>
2024-01-28 11:00:58 +0530

    seprated order based on stage

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from '../firebaseinit';
 
 export default function Mainpage() {
+    const [orders,setOrders]=useState([]);
+    useEffect(()=>{
+        onSnapshot(collection(db,"pizzashop"), (snapshot) => {
+       
+            const orders =snapshot.docs.map((doc) => {
+             return {id:doc.id,
+              ...doc.data()}
+            }) 
+           setOrders(orders);
+           console.log(orders);
+         })
+},[]);
+     //seprated order based on there stage
+   const orderplaced=orders.filter((order)=>{
+    if(order.orderstage==1){
+        return order;
+    }
+   })
+   console.log("placed",orderplaced);
+   const ordermaking=orders.filter((order)=>{
+    if(order.orderstage==2){
+        return order;
+    }
+   })
+   console.log("making",ordermaking);
+   const orderready=orders.filter((order)=>{
+    if(order.orderstage==3){
+        return order;
+    }
+   })
+   const orderpicked=orders.filter((order)=>{
+    if(order.orderstage==4){
+        return order;
+    }
+   })
+   
+    
   return (
-    <div>Mainpage</div>
+    <div>
+       
+        
+        <div className="ordermaking"></div>
+        <div className="orderready"></div>
+        <div className="orderpicked"></div>
+    </div>
   )
 }
